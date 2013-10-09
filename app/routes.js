448c2fb248ecf24e8f77be37f6f6393c9fbbae5f
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-10-09 16:21:27 +0200

    Seperated routes into standalone file and fixed angular-cookies

---
+++ app/routes.js
@@ -0,0 +1,25 @@
+app.config(function($routeProvider) {
+	$routeProvider.when('/veranstaltung', {
+		templateUrl: 'app/views/all.html',
+		controller: 'AllController'
+	});
+
+	$routeProvider.when('/veranstaltung/new', {
+		templateUrl: 'app/views/new.html',
+		controller: 'NewController'
+	});
+
+	$routeProvider.when('/veranstaltung/delete/:id', {
+		templateUrl: 'app/views/delete.html',
+		controller: 'DeleteController'
+	});
+
+	$routeProvider.when('/veranstaltung/:id', {
+		templateUrl: 'app/views/single.html',
+		controller: 'SingleController'
+	});
+
+	
+
+	$routeProvider.otherwise({ redirectTo: '/veranstaltung/new'});
+});
