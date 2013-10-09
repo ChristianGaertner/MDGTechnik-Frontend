a2fd039af3e8c17f97b6747d557ac8201b7268d9
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-10-09 17:25:11 +0200

    further editing

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,5 +19,5 @@ module.exports = function (grunt) {
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['jshint', 'uglify']);
 };
