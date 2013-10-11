c1a67b2869a6b346682dc8a36d6b80de2b7b3f42
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-10-11 12:57:48 +0200

    Adding livereload and API_ADRESS is now pointing to localhost if the frontend runs on localhost

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,25 +7,51 @@ module.exports = function (grunt) {
 			all: ['app/**/*.js']
 		},
 
+		ngmin: {
+			app: {
+				src: ['app/*.js'],
+				dest: 'dist/app/app.js'
+			},
+			controllers: {
+				src: ['app/controller/*.js'],
+				dest: 'dist/app/controllers.js'
+			},
+			directives: {
+				src: ['app/directives/*.js'],
+				dest: 'dist/app/directives.js'
+			},
+			services: {
+				src: ['app/services/*.js'],
+				dest: 'dist/app/services.js'
+			},
+			config: {
+				src: ['app/config/*.js'],
+				dest: 'dist/app/config.js'
+			}
+		},
+
 		uglify: {
 			dist: {
-				src: 'app/**/*.js',
+				src: 'dist/app/*.js',
 				dest: 'dist/app.min.js'
 			},
 			options: {
-				banner: '/*\n * (c) Christian Gärtner 2013\n * MDGTechnik AngularJS Frontend.\n * License: All rights reserved\n * Build on <%= grunt.template.today("yyyy-mm-dd") %>\n */\n',
-				mangle: false
+				banner: '/*\n * (c) Christian Gärtner 2013\n * MDGTechnik AngularJS Frontend.\n * License: All rights reserved\n * Build on <%= grunt.template.today("yyyy-mm-dd") %>\n */\n'
 			}
 		},
 
 		watch: {
-			files: ['app/**/*.js'],
-			tasks: ['jshint', 'uglify']
+			files: ['app/**/*.js', 'app/views/*.html', 'index.html'],
+			tasks: ['jshint', 'ngmin', 'uglify'],
+			options: {
+				livereload: true
+			}
 		},
 	});
+	grunt.loadNpmTasks('grunt-ngmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.registerTask('default', ['jshint', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'ngmin', 'uglify']);
 };
--- app/config/apiConnection.js
+++ app/config/apiConnection.js
@@ -1,3 +1,10 @@
 var API_VERSION = 'v1';
-var API_ADRESS = 'http://api.mdg-technik.tk/' + API_VERSION + '/';
-// var API_ADRESS = 'http://192.168.2.107:4000/' + API_VERSION + '/';
+
+var API_ADRESS;
+if (window.location.host.indexOf('localhost') == -1) {
+	API_ADRESS = 'http://api.mdg-technik.tk/' + API_VERSION + '/';	
+} else {
+	API_ADRESS = 'http://192.168.2.107:4000/' + API_VERSION + '/';
+}
+
+console.log(API_ADRESS);
