d076d8f8d6f7ba3bc9aa9a98240ad6614bf80771
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-10-11 14:53:37 +0200

    Finishing deletion. Almost done

--- app/controller/delete.js
+++ app/controller/delete.js
@@ -1,3 +1,38 @@
-angular.module('app').controller('DeleteController', function($scope, $http, $location, $routeParams) {
+angular.module('app').controller('DeleteController', function($scope, $http, $location, $routeParams, NotificationService) {
     $scope.id = $routeParams.id;
+
+    $scope.delete = function() {
+        $http({
+            url: API_ADRESS + 'veranstaltung/' + $scope.id + '?key=' + window.btoa($scope.key),
+            method: 'DELETE',
+            data: $scope.key,
+        })
+        .success(function(res) {
+            NotificationService.show('Hinweis', 'Die Veranstaltung wurde erfolgreich gelöscht');
+            $location.path('veranstaltungen');
+        })
+        .error(function(res, status) {
+            console.log(res);
+            if (status == 403) {
+                NotificationService.show(
+                    'Hinweis',
+                    'Anscheinend wurde keine ID übermittelt überprüfen Sie ihre URL.' + 
+                    '<br /> Sie muss in etwas so aussehen: <code>/delete/1234</code>'
+                );
+            } else if(status == 401) {
+                if (res.message == 'Please provide the key for this event') {
+                    NotificationService.show(
+                        'Achtung!',
+                        'Bitte geben sie einen Schlüssel ein!'
+                        );
+                } else {
+                    NotificationService.show(
+                        'Achtung!',
+                        'Der eingegebene Schlüssel ist nicht valide!'
+                        );
+                }
+            }
+        });
+    };
+
 });
